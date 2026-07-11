07d32f10bec7bf630a2460d1f305d97300f366fe
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-07-11 20:37:39 +0530

    Time: 12 ms (95.3%), Space: 139.2 MB (67.98%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -7,11 +7,12 @@ public:
             if(!dq.empty() && dq.front()<=i-k){
                 dq.pop_front();
             }
-            while(!dq.empty() && nums[dq.back()]< nums[i]){
+            while(!dq.empty() && nums[dq.back()]<nums[i]){
                 dq.pop_back();
             }
             dq.push_back(i);
-            if(i>= k-1){
+        
+            if(i>=k-1){
                 ans.push_back(nums[dq.front()]);
             }
         }
