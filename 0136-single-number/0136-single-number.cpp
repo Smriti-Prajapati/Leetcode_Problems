4c228b2cdb8dfb080de729e618c34ee973cbd4b5
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-07-15 15:14:56 +0530

    Time: 0 ms (100%), Space: 20.6 MB (58.87%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        int n= nums.size();
         int XOR=0;
-        for(int i=0; i<nums.size(); i++){
-            XOR= XOR ^nums[i];
+        for(int i=0; i<n; i++){
+            XOR= XOR^ nums[i];
         }
         return XOR;
     }
