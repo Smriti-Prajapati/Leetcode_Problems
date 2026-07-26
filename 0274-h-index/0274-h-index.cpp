dbe94185d8fc514115fdc5d22c4a200d7b68f95a
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-07-26 13:24:47 +0530

    Time: 0 ms (100%), Space: 12.1 MB (86.05%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -4,8 +4,8 @@ public:
         sort(citations.begin(), citations.end(), greater<int>());
         int h=0;
         for(int i=0; i<citations.size(); i++){
-            if(citations[i]>=i+1){
-                h=i+1;
+            if(citations[i]>= i+1){
+                h= i+1;
             }
             else{
                 break;
