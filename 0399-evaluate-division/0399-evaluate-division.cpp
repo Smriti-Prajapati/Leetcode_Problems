2cefca1ef61e3722c9cd17e90e53b9a3e4db76bb
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-07-29 19:18:41 +0530

    Time: 0 ms (100%), Space: 12.2 MB (13.39%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    double dfs(string src, string dest, unordered_map<string, vector<pair<string ,double>>> &adj, unordered_map<string, bool> &vis, double product){
+    double dfs(string src, string dest, unordered_map<string, vector<pair<string, double>>> & adj, unordered_map<string, bool> &vis, double product){
         if(src== dest)
             return product;
-        vis[src]=true;
+        vis[src]= true;
         for(auto it: adj[src]){
             string next= it.first;
             double value= it.second;
             if(!vis[next]){
-                double ans= dfs(next, dest, adj, vis, product*value);
-                if(ans!= -1.0){
+                double ans= dfs(next, dest, adj, vis, product* value);
+                if(ans != -1.0){
                     return ans;
                 }
             }
@@ -19,16 +19,16 @@ public:
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
         unordered_map<string, vector<pair<string, double>>> adj;
         for(int i=0; i<equations.size(); i++){
-            string u = equations[i][0];
+            string u= equations[i][0];
             string v= equations[i][1];
             adj[u].push_back({v, values[i]});
-            adj[v].push_back({u,1.0/values[i]});
+            adj[v].push_back({u, 1.0/values[i]});
         }
         vector<double> res;
         for(auto q: queries){
             string src= q[0];
             string dest= q[1];
-            if(adj.find(src)== adj.end() || adj.find(dest)==adj.end()){
+            if(adj.find(src)== adj.end() || adj.find(dest)== adj.end()){
                 res.push_back(-1.0);
                 continue;
             }
