11318fab207d0df6037d93c9846d8a4ab7133cce
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-07-31 01:07:20 +0530

    Time: 0 ms (100%), Space: 23.6 MB (11.75%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n= ratings.size();
-        vector<int> left(n, 1);
+        vector<int> left(n,1);
         vector<int> right(n,1);
         left[0]=1;
         right[n-1]=1;
@@ -14,8 +14,8 @@ public:
                 left[i]=1;
             }
         }
-        for(int i= n-2; i>=0; i--){
-            if(ratings[i]> ratings[i+1]){
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i]>ratings[i+1]){
                 right[i]= right[i+1]+1;
             }
             else{
