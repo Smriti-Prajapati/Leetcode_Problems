27af8d6f602a4e1d9481184f04224289d4fc9d2e
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-07-30 14:38:36 +0530

    Time: 0 ms (100%), Space: 8.2 MB (77.73%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int minimumPushes(string word) {
         int ans=0;
-        for(int i=0; i<word.length();i++){
-            ans += i/8+1;
+        for(int i=0; i<word.length(); i++){
+            ans+= i/8+1;
         }
         return ans;
     }
