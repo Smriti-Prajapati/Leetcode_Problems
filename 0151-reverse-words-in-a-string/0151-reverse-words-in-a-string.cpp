01881b272676d0fbd5c99672cd15dc8cc95714c6
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-08-06 19:17:04 +0530

    Time: 2 ms (40.15%), Space: 11.2 MB (21.44%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -4,20 +4,15 @@ public:
         vector<string> words;
         string word;
         stringstream ss(s);
-
-        while (ss >> word) {
+        while(ss>>word){
             words.push_back(word);
         }
-
-        string ans = "";
-
-        for (int i = words.size() - 1; i >= 0; i--) {
+        string ans="";
+        for(int i=words.size()-1;i>=0;i--){
             ans += words[i];
-
-            if (i != 0)
-                ans += " ";
+            if(i != 0)
+                ans+=" ";
         }
-
         return ans;
     }
 };
