d2cf1986fae9356ec9de9573bea95a5cf9e49228
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-08-12 16:38:35 +0530

    Time: 0 ms (100%), Space: 41.8 MB (67.59%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -4,18 +4,20 @@ public:
         int n= nums.size();
         int left=0;
         int sum=0;
-        int ans=INT_MAX;
-        for(int right=0;right<n;right++){
-            sum+= nums[right];
+        int ans= INT_MAX;
+        for(int right=0; right<n;right++){
+            sum+=nums[right];
             while(sum>=target){
                 ans= min(ans, right-left+1);
-                sum-= nums[left];
+                sum-=nums[left];
                 left++;
             }
         }
         if(ans==INT_MAX){
             return 0;
         }
-        return ans;
+        else{
+            return ans;
+        }
     }
 };
