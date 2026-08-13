2493e4b7acd2ea179f7049c8ee9b698047b025e8
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-08-13 20:15:03 +0530

    Time: 1 ms (20.56%), Space: 13.3 MB (45.82%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,9 +3,10 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n= triangle.size();
         vector<vector<int>> dp= triangle;
+        //i=n-2 because 0 based imdexing is there
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<=i;j++){
-                dp[i][j]=triangle[i][j]+min(dp[i+1][j], dp[i+1][j+1]);
+                dp[i][j]= triangle[i][j]+ min(dp[i+1][j], dp[i+1][j+1]);
             }
         }
         return dp[0][0];
