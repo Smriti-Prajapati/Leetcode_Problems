c3a45ab3493c829e4e3dba63e2ec4c209e94b19e
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-08-25 20:44:41 +0530

    Time: 0 ms (100%), Space: 9.6 MB (75.8%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int hash[26];
+        fill(hash,hash+26,0);
+        for(int i=0;i<s.length();i++){
+            hash[s[i]-'a']++;
+            hash[t[i]-'a']--;
+        }
+        for (int i=0;i<26;i++){
+            if(hash[i]!= 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
