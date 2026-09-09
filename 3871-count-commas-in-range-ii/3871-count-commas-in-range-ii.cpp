4660841fe31a62be31c4c9522a957582af596ed1
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-09-09 13:50:40 +0530

    Time: 0 ms (100%), Space: 9 MB (57.33%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -4,7 +4,7 @@ public:
         long long ans=0;
         long long power=1000;
         while(power<=n){
-            ans+= n-power+1; //n-power+1
+            ans+= n-power+1; //ans+=n-power+1
             power*=1000;
         }
         return ans;
