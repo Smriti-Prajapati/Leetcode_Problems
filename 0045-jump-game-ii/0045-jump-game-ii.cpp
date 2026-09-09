9701702c2c52d83bb874a5e5b53ae756bda22024
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-09-09 21:40:34 +0530

    Time: 0 ms (100%), Space: 20.6 MB (78.59%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -5,13 +5,12 @@ public:
         int currentEnd=0;
         int farthest=0;
         for(int i=0;i<nums.size()-1;i++){
-            farthest = max(farthest, i+nums[i]);
+            farthest= max(farthest, i+nums[i]);
             if(i==currentEnd){
                 jumps++;
-                currentEnd= farthest;
+                currentEnd=farthest;
             }
         }
         return jumps;
-
     }
 };
