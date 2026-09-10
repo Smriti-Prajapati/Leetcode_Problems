bf31e1e504a978dc29a88d63b4e937e664526288
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-09-10 20:06:36 +0530

    Time: 3 ms (91.06%), Space: 15.8 MB (64.72%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -18,8 +18,8 @@ public:
         }
         auto left=dfs(node->left);
         auto right=dfs(node->right);
-        int sum= left.first+ right.first+ node->val;
-        int count=left.second+right.second+1;
+        int sum= left.first+ right.first+ node->val; //subtree sum
+        int count=left.second+right.second+1; //count suntree size
         if(node->val== sum/count){  //node.val= sum/count
             answer++;
         }
