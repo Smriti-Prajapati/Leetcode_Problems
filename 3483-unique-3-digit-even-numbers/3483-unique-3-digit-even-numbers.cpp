a7ea5197f09f27a83453ed16da16ce76bc62fc88
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-09-11 14:50:44 +0530

    Time: 13 ms (27.68%), Space: 28.7 MB (88.78%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        int freq[10]={};
+        for(int d:digits){
+            freq[d]++;
+        }
+        int ans=0;
+        for(int num=100;num<999;num++){
+            if(num%2!=0){
+                continue;
+            }
+            int a=num/100;
+            int b=(num/10)%10;
+            int c=num%10;
+            int need[10]={};
+            need[a]++;
+            need[b]++;
+            need[c]++;
+            bool possible=true;
+            for(int d=0;d<10;d++){
+                if(need[d]>freq[d]){
+                    possible=false;
+                    break;
+                }
+            }
+            if(possible)
+                ans++;
+        }
+        return ans;
+    }
+};
