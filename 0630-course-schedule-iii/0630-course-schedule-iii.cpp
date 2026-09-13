a604ca70309fcb8cdc55667894296f98dc0446c9
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-09-14 01:27:09 +0530

    Time: 60 ms (22.15%), Space: 67.6 MB (26.63%) - LeetHub

---
+++ 0630-course-schedule-iii/0630-course-schedule-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        //step 1.sort the courses in ascending order of deadline
+        sort(courses.begin(),courses.end(),
+        [](const vector<int>& a, vector<int>& b){
+            return a[1]<b[1];
+        });
+        //step 2.create a max heap
+        priority_queue<int> pq;
+        int time=0;
+        for(auto course: courses){
+            int duration=course[0];
+            int deadline=course[1];
+            time+=duration;
+            pq.push(duration);
+            if(time>deadline){
+                time-=pq.top();
+                pq.pop();
+            }
+        }
+        return pq.size(); //number of courses we are taken will be returned as answer
+    }
+};
