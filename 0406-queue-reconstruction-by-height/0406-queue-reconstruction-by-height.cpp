fc831b8707e737ceaa68a87bd3eec8dadc5bf02e
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-09-14 00:28:07 +0530

    Time: 18 ms (66.35%), Space: 16.4 MB (29.19%) - LeetHub

--- 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.cpp
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        //step 1.firstly we will sort the peoples array as required h descending and k ascending
-        sort(people.begin(), people.end(),[](const vector<int>& a, vector<int>& b){
+        sort(people.begin(), people.end(),
+        [](const vector<int>& a, const vector<int>& b){
             if(a[0]==b[0])
-                return a[1]<b[1]; //k ascending
-            return a[0]>b[0]; //h desending
+                return a[1]<b[1];
+            return a[0]>b[0];
         });
         vector<vector<int>> ans;
-        for(auto person: people){
-            ans.insert(ans.begin()+person[1],person); //person[1] tells where to insert i.e. after how many position
+        for(auto person:people){
+            ans.insert(ans.begin()+person[1],person);
         }
         return ans;
     }
