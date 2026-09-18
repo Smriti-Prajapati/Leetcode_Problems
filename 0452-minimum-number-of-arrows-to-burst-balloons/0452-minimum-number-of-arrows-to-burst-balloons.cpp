e1abab4eb2f4378900503a108d91adaf253b78a4
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-09-18 21:33:58 +0530

    Time: 40 ms (87.48%), Space: 94 MB (27.9%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
-        //step 1. is we will sort the array in descending order but by end points as we want the maximum balloons sto be burst at once;
         sort(points.begin(),points.end(),
-        [](const vector<int>&a, const vector<int>& b){
-            return a[1]< b[1];
+        [](const vector<int>& a, const vector<int>& b){
+            return a[1]<b[1];
         });
-        //step 2.initialize arrrow as 1 and arrow position at first point end
         int arrows=1;
-        int arrowPos= points[0][1];
-        //step 3.iterate
+        int arrowsPos=points[0][1];
         for(int i=1;i<points.size();i++){
-            if(points[i][0]>arrowPos){
+            if(points[i][0]>arrowsPos){
                 arrows++;
-                arrowPos= points[i][1];
+                arrowsPos=points[i][1];
             }
         }
         return arrows;
