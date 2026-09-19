8b4c1f7146a1733c79c7632f617fa789ac872ad5
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-09-19 22:15:03 +0530

    Time: 0 ms (100%), Space: 42.3 MB (5.73%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        //by boyer-moore voting algorithm
         int candidate=0;
         int count=0;
         for(int x:nums){
