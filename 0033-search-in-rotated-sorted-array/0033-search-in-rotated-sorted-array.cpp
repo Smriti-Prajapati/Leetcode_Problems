9dd847f93719484e706df29874d8762c1c193e31
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-09-20 01:16:52 +0530

    Time: 0 ms (100%), Space: 15.1 MB (69.94%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -3,24 +3,22 @@ public:
     int search(vector<int>& nums, int target) {
         int left=0;
         int right=nums.size()-1;
-        while(left<= right){
-            int mid= left+ (right-left)/2;
-            if(nums[mid]== target){
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
                 return mid;
             }
-            //left part sorted
-            if(nums[left]<= nums[mid]){
-                if(nums[left]<= target && target<=nums[mid]){
-                    right= mid-1;
+            else if(nums[left]<=nums[mid]){
+                if(nums[left]<=target && target<=nums[mid]){
+                    right=mid-1;
                 }
                 else{
-                    left= mid+1;
+                    left=mid+1;
                 }
             }
-            //right part sorted
             else{
-                if(nums[mid]<= target && target<=nums[right]){
-                    left= mid+1;
+                if(nums[mid]<=target && target<=nums[right]){
+                    left=mid+1;
                 }
                 else{
                     right=mid-1;
@@ -30,3 +28,4 @@ public:
         return -1;
     }
 };
+
