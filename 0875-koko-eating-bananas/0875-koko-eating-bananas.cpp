7534f30bc2570688f7aa44a4357cd8cc7dd13fcf
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-09-24 00:13:41 +0530

    Time: 11 ms (35.21%), Space: 23 MB (58.41%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int left=1;
-        int right=*max_element(piles.begin(), piles.end());
-        //bianry search
-        while(left< right){
-            int k= left+(right- left)/2;
+        int left=1; //as koko can eat atleast 1 banana
+        int right=*max_element(piles.begin(),piles.end());
+        //apply binary search
+        while(left<right){
+            int mid=left+(right-left)/2;
             long long hours=0;
-            for(int pile : piles){
-                hours+= (pile+k-1)/k; //ceil in cpp= (arr+k-1)/k;
+            for(int pile: piles){
+                hours+=(pile+mid-1)/mid;
             }
             if(hours<=h){
-                right=k;
+                right=mid;
             }
             else{
-                left=k+1;
+                left=mid+1;
             }
         }
         return left;
