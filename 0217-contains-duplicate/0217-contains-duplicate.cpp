899830f39120d397fc34307075856738d106a838
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-09-24 21:26:54 +0530

    Time: 86 ms (18.2%), Space: 113.5 MB (16.32%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> freq;
+        for(int num:nums){
+            freq[num]++;
+        }
+        for(auto &p:freq){
+            if(p.second>1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
