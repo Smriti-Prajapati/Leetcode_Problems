51cf1661782139571c51bb6d6fcf74e146be526a
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-09-25 01:19:27 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.06%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int k=0;
+        int k=0; //k->writes, i->reads
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=0){
                 nums[k]=nums[i];
                 k++;
             }
         }
+        //fill remaining position with 0's
         while(k<nums.size()){
             nums[k]=0;
             k++;
